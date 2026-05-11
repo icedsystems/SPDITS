48e45c99c803a7b08e1a949d06e51a3cf10f9f6a
icedsystems <icedgithub@gmail.com>
2026-05-11 21:01:21 +0300

    Add CSRF_TRUSTED_ORIGINS for ea.data.iced-eval.org

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,9 @@ from .base import *
 
 DEBUG = False
 
+APP_URL = env('APP_URL', default='')
+CSRF_TRUSTED_ORIGINS = [u for u in [APP_URL, f'https://ea.data.iced-eval.org'] if u]
+
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True

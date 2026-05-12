7d4feaf7e4180c46a8d8d3b8b5dfff78ad6d6e10
icedsystems <icedgithub@gmail.com>
2026-05-12 09:28:54 +0300

    feat: email notification settings UI with Graph API config

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.NotificationListView.as_view(), name='list'),
     path('<int:pk>/read/', views.NotificationMarkReadView.as_view(), name='mark_read'),
     path('read-all/', views.NotificationMarkAllReadView.as_view(), name='mark_all_read'),
+    path('email-config/', views.EmailConfigView.as_view(), name='email_config'),
 ]

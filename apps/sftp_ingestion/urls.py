be4e40ac85c800491d1b87873c4dc97817388830
icedsystems <icedgithub@gmail.com>
2026-05-20 16:33:16 +0300

    fix: invitations empty row, SFTP redirect, change-password URL + template

--- apps/sftp_ingestion/urls.py
+++ apps/sftp_ingestion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 app_name = 'sftp'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/sftp/config/', permanent=False)),
     path('logs/', views.SFTPLogListView.as_view(), name='log_list'),
     path('config/', views.SFTPConfigListView.as_view(), name='config_list'),
     path('config/new/', views.SFTPConfigCreateView.as_view(), name='config_create'),
